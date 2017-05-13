d6eef90f9e06f9157362ce7c7cf0fa651cabbff9
Sagar Jha <sjsagarjha3@gmail.com>
2017-05-13 19:50:11 -0400

    Error handling when variable eshell-list is not defined

--- multi-eshell-run.el
+++ multi-eshell-run.el
@@ -17,23 +17,22 @@
   (setq evaled-command command)
   (while names
     (when (functionp command)
-      (setq evaled-command (funcall command loop-cnt)
-	    ))
+      (setq evaled-command (funcall command loop-cnt)))
     (run-at-time (concat (number-to-string (* delay-cnt delay)) " sec") nil 
 		 'run-on-eshell-terminal evaled-command (car names))
     (setq names (cdr names))
     (setq delay-cnt (+ 1 delay-cnt))
     (setq loop-cnt (+ 1 loop-cnt))))
 
-(setq window-batch 5)
+(setq eshell-window-batch 5)
 
 (defun open-eshell-terminals (num-terminals)
   (delete-other-windows)
-  (dotimes (i (+ 1 (/ (+ -1 num-terminals) window-batch)))
+  (dotimes (i (+ 1 (/ (+ -1 num-terminals) eshell-window-batch)))
     (split-window-horizontally)
     (balance-windows))
   (dotimes (i num-terminals)
-    (if (= (% i window-batch) 0)
+    (if (= (% i eshell-window-batch) 0)
 	(other-window 1)
       (progn (split-window-vertically)
 	     (balance-windows)
@@ -42,12 +41,16 @@
   (other-window 1))
 
 (defun run (&rest cmd)
+  (when (not (boundp (quote eshell-list)))
+    (error "Define the variable eshell-list on which you want to run the command on. e.g. (list 1 2 3)"))
   (mapc
    (lambda (command) (run-on-multiple-eshell-terminals command eshell-list))
    cmd)
   nil)
 
 (defun run-with-delay (delay cmd)
+  (when (not (boundp (quote eshell-list)))
+    (error "Define the variable eshell-list on which you want to run the command on. e.g. (list 1 2 3)"))
   (run-on-multiple-eshell-terminals cmd eshell-list delay))
 
 (provide 'multi-eshell-run)
