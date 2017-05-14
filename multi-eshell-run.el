c11509d89d5bc4ce7e8128071aa3f05bb6769875
Sagar Jha <sjsagarjha3@gmail.com>
2017-05-13 20:22:32 -0400

    run-ssh works!

--- multi-eshell-run.el
+++ multi-eshell-run.el
@@ -13,20 +13,18 @@
   (when (not delay)
     (setq delay 0))
   (setq delay-cnt 0)
-  (setq loop-cnt 1)
   (setq evaled-command command)
   (while names
     (when (functionp command)
-      (setq evaled-command (funcall command loop-cnt)))
+      (setq evaled-command (funcall command (car names))))
     (run-at-time (concat (number-to-string (* delay-cnt delay)) " sec") nil 
 		 'run-on-eshell-terminal evaled-command (car names))
     (setq names (cdr names))
-    (setq delay-cnt (+ 1 delay-cnt))
-    (setq loop-cnt (+ 1 loop-cnt))))
+    (setq delay-cnt (+ 1 delay-cnt))))
 
 (setq eshell-window-batch 5)
 
-(defun open-eshell-terminals (num-terminals)
+(defun configure-eshell-terminals (num-terminals)
   (delete-other-windows)
   (dotimes (i (+ 1 (/ (+ -1 num-terminals) eshell-window-batch)))
     (split-window-horizontally)
@@ -38,7 +36,10 @@
 	     (balance-windows)
 	     (other-window 1)))
     (eshell (+ i 1)))
-  (other-window 1))
+  (other-window 1)
+  (setq eshell-list (number-sequence 1 num-terminals))
+  (print (concat "Preemptively setting the eshell-list to " (prin1-to-string eshell-list)))
+  nil)
 
 (defun run (&rest cmd)
   (when (not (boundp (quote eshell-list)))
@@ -52,5 +53,13 @@
   (when (not (boundp (quote eshell-list)))
     (error "Define the variable eshell-list on which you want to run the command on. e.g. (list 1 2 3)"))
   (run-on-multiple-eshell-terminals cmd eshell-list delay))
+
+;; convenience function for ssh'ing to the terminals
+(defun run-ssh ()
+  (when (not (boundp (quote eshell-list)))
+    (error "Define the variable eshell-list on which you want to run the command on. e.g. (list 1 2 3)"))
+  (when (not (boundp (quote nodes-list)))
+    (error "Define the variable nodes-list to be the list of ip-addrs e.g. (list \"128.84.139.10\" \"128.84.139.11\" \"128.84.139.12\")"))
+  (run-on-multiple-eshell-terminals (lambda (x) (concat "ssh " (elt nodes-list (- x 1)))) eshell-list))
 
 (provide 'multi-eshell-run)
