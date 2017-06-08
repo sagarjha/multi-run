89800609b4c5d47451e3d8396072fbcb58e67e79
Sagar Jha <sjsagarjha3@gmail.com>
2017-06-08 15:15:06 -0400

    Renamed the functions to have multi-run- or mr- as the prefix
    This is to avoid namespace collisions. I have to verify that all
    functions work i.e. I didn't forget to rename the function calls as well

--- multi-eshell-run.el
+++ multi-eshell-run.el
@@ -3,14 +3,14 @@
 (setq multi-run-timers (list))
 
 ;; run a command on a single eshell terminal
-(defun run-on-eshell-terminal (command name)
+(defun mr-run-on-single-eshell-terminal (command name)
   (set-buffer (concat "*eshell*<" (number-to-string name) ">"))
   (goto-char (point-max))
   (insert command)
   (eshell-send-input))
 
 ;; run a command on multiple eshell terminals - useful for ssh
-(defun run-on-multiple-eshell-terminals (command names &optional delay)
+(defun mr-run-on-eshell-terminals (command names &optional delay)
   "Run a command on multiple eshell terminals - useful for ssh"
   (when (not delay)
     (setq delay 0))
@@ -20,11 +20,11 @@
     (when (functionp command)
       (setq evaled-command (funcall command (car names))))
     (setq multi-run-timers (cons (run-at-time (concat (number-to-string (* delay-cnt delay)) " sec") nil 
-					      'run-on-eshell-terminal evaled-command (car names)) multi-run-timers))
+					      'mr-run-on-single-eshell-terminal evaled-command (car names)) multi-run-timers))
     (setq names (cdr names))
     (setq delay-cnt (1+ delay-cnt))))
 
-(defun configure-eshell-terminals (num-terminals &optional eshell-window-batch)
+(defun mr-configure-eshell-terminals (num-terminals &optional eshell-window-batch)
   (when (not eshell-window-batch)
     (setq eshell-window-batch 5))
   (delete-other-windows)
@@ -42,21 +42,21 @@
   (setq eshell-list (number-sequence 1 num-terminals))
   (concat "Preemptively setting the eshell-list to " (prin1-to-string eshell-list)))
 
-(defun run (&rest cmd)
+(defun multi-run (&rest cmd)
   (interactive)
   (when (not (boundp (quote eshell-list)))
     (error "Define the variable eshell-list on which you want to run the command on. e.g. (list 1 2 3)"))
   (mapc
-   (lambda (command) (run-on-multiple-eshell-terminals command eshell-list))
+   (lambda (command) (mr-run-on-eshell-terminals command eshell-list))
    cmd)
-  nil)
+  cmd)
 
-(defun run-with-delay (delay cmd)
+(defun multi-run-with-delay (delay cmd)
   (when (not (boundp (quote eshell-list)))
     (error "Define the variable eshell-list on which you want to run the command on. e.g. (list 1 2 3)"))
-  (run-on-multiple-eshell-terminals cmd eshell-list delay))
+  (mr-run-on-eshell-terminals cmd eshell-list delay))
 
-(defun run-loop (cmd &optional times delay)
+(defun multi-run-loop (cmd &optional times delay)
   (when (not times)
     (setq times 1))
   (when (not delay)
@@ -65,18 +65,18 @@
   (dotimes (i times)
     (progn 
       (setq multi-run-timers (cons (run-at-time (concat (number-to-string (* delay-cnt delay)) " sec")
-						nil '(lambda (cmd) (run cmd)) cmd) multi-run-timers))
+						nil '(lambda (cmd) (multi-run cmd)) cmd) multi-run-timers))
       (setq delay-cnt (1+ delay-cnt)))))
 
 ;; convenience function for ssh'ing to the terminals
-(defun run-ssh (&optional eshell-num)
+(defun multi-run-ssh (&optional eshell-num)
   (when (not (boundp (quote eshell-list)))
     (error "Define the variable eshell-list on which you want to run the command on. e.g. (list 1 2 3)"))
   (when (not (boundp (quote nodes-list)))
     (error "Define the variable nodes-list to be the list of ip-addrs e.g. (list \"128.84.139.10\" \"128.84.139.11\" \"128.84.139.12\")"))
-  (run-on-multiple-eshell-terminals (lambda (x) (concat "ssh " (if (boundp (quote ssh-username)) (concat ssh-username "@") "") (elt nodes-list (- x 1)))) (if eshell-num (list eshell-num) eshell-list)))
+  (mr-run-on-eshell-terminals (lambda (x) (concat "ssh " (if (boundp (quote ssh-username)) (concat ssh-username "@") "") (elt nodes-list (- x 1)))) (if eshell-num (list eshell-num) eshell-list)))
 
-(defun multi-run-kill (&optional eshell-num)
+(defun multi-run-kill-terminals (&optional eshell-num)
   (when (not (boundp (quote eshell-list)))
     (error "Define the variable eshell-list on which you want to run the command on. e.g. (list 1 2 3)"))
   (if eshell-num
@@ -84,7 +84,7 @@
     (mapc (lambda (name) (kill-buffer (concat "*eshell*<" (number-to-string name) ">"))) eshell-list))
   nil)
 
-(defun multi-run-kill-timers ()
+(defun multi-run-kill-all-timers ()
   (mapc (lambda (timer) (cancel-timer timer)) multi-run-timers)
   "All timers canceled")
 
