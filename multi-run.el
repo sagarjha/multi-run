2fbfc43fb05839724b9a916d3cde210d314f4975
Sagar <sjsagarjha3@gmail.com>
2017-11-30 22:47:03 -0500

    multi-run calls multi-run-with-delay 0 reducing code copy

--- multi-run.el
+++ multi-run.el
@@ -130,14 +130,8 @@
   (concat "Preemptively setting mr-terminals to " (prin1-to-string mr-terminals)))
 
 (defun multi-run (&rest cmd)
-  "Runs one or more commands on multiple terminals"
-  (interactive)
-  (when (not (boundp (quote mr-terminals)))
-    (error "Define the variable mr-terminals on which you want to run the command on. e.g. (list 1 2 3)"))
-  (mapc
-   (lambda (command) (mr-run-on-terminals command mr-terminals))
-   cmd)
-  nil)
+  "Runs one or more commands on multiple terminals" 
+  (apply #'multi-run-with-delay 0 cmd))
 
 (defun multi-run-with-delay (delay &rest cmd)
   "Runs one or more commands on multiple terminals with the provided delay"
@@ -146,7 +140,8 @@
   (setq delay-now 0)
   (dolist (command cmd)
     (setq multi-run-timers (cons (run-at-time (concat (number-to-string delay-now) " sec") nil 'mr-run-on-terminals command mr-terminals delay) multi-run-timers))
-    (setq delay-now (+ delay-now (* (length mr-terminals) delay)))))
+    (setq delay-now (+ delay-now (* (length mr-terminals) delay))))
+  nil)
 
 (defun multi-run-loop (cmd &optional times delay)
   "Loops the command given number of times with delay between successive runs"
