0865b62230766b8919dfc2e86d9c1dcc90941905
Sagar <sjsagarjha3@gmail.com>
2017-12-23 21:32:33 -0500

    Formatted code according to Steve's comments on melpa pull request
    https://github.com/melpa/melpa/pull/5192 for more details

--- multi-run.el
+++ multi-run.el
@@ -1,115 +1,152 @@
-;;; multi-run --- A minor mode for managing multiple emacs eshell terminals and running commands on them
+;;; multi-run.el --- Manage multiple terminals and run commands on them
+
+;; Copyright (C) 2017  Sagar Jha
+
+;; Author: Sagar Jha
+;; URL: https://www.github.com/sagarjha/multi-run
+;; Package-Requires: ((emacs "24") (window-layout "1.4"))
+;; Version: 1.0
+;; Keywords: tools, terminals
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
-;;; The functions below are self-explanatory with the documentation string.
+;; The functions below are self-explanatory with the documentation string.
 
-;;; See the full documentation on https://www.github.com/sagarjha/multi-run
+;; See the full documentation on https://www.github.com/sagarjha/multi-run
 
 ;;; Code:
 
-(provide 'multi-run)
-
 (require 'window-layout)
 
-(defvar mr-term "eshell"
+(defvar multi-run-term-type 'eshell
   "Terminal type to run the commands on.")
-;; (setq mr-term "eshell")
-;; (setq mr-term "shell")
-;; (setq mr-term "ansi-term")
-;; (setq mr-term "term")
-;; (setq mr-term "multi-term")
-(setq multi-run-timers (list))
+;; set multi-run-term-type to any of the supported terminals by running the appropriate expression from below
+;; (setq multi-run-term-type 'eshell)
+;; (setq multi-run-term-type 'shell)
+;; (setq multi-run-term-type 'ansi-term)
+;; (setq multi-run-term-type 'term)
+;; (setq multi-run-term-type 'multi-term)
 
-(defun mr-get-buffer-name (term-num)
+(defvar multi-run-timers-list ((list))
+  "Internal list of timers to cancel when multi-run-kill-all-timers is called.")
+
+(defun multi-run-match-term-type-p (multi-run-term-type)
+  "Return true if MULTI-RUN-TERM-TYPE is one of the supported terminal types."
+  (pcase multi-run-term-type
+    ('eshell 't)
+    ('shell 't)
+    ('ansi-term 't)
+    ('term 't)
+    ('multi-term 't)))
+
+(defun multi-run-get-buffer-name (term-num)
   "Return the name of the buffer for a given terminal number TERM-NUM."
-  (cond ((string= mr-term "eshell") (concat "eshell<" (number-to-string term-num) ">"))
-	((string= mr-term "shell") (concat "shell<" (number-to-string term-num) ">"))
-	((string= mr-term "ansi-term") (concat "ansi-term<" (number-to-string term-num) ">"))
-	((string= mr-term "term") (concat "term<" (number-to-string term-num) ">"))
-	((string= mr-term "multi-term") (concat "terminal<" (number-to-string term-num) ">"))))
+  (let ((term-num-in-str (number-to-string term-num)))
+    (pcase multi-run-term-type
+      ('eshell (concat "eshell<" term-num-in-str ">"))
+      ('shell (concat "shell<" term-num-in-str ">"))
+      ('ansi-term (concat "ansi-term<" term-num-in-str ">"))
+      ('term (concat "term<" term-num-in-str ">"))
+      ('multi-term (concat "terminal<" term-num-in-str ">"))
+      (multi-run-term-type (error "Value of multi-run-term-type should be one of the following symbols: eshell, shell, ansi-term, term, multi-term")))))
 
-(defun mr-get-input-function ()
+(defun multi-run-get-input-function ()
   "Return the name of the function that will run the input on the terminal."
-  (cond ((string= mr-term "eshell") 'eshell-send-input)
-	((string= mr-term "shell") 'comint-send-input)
-	((string= mr-term "ansi-term") 'term-send-input)
-	((string= mr-term "term") 'term-send-input)
-	((string= mr-term "multi-term") 'term-send-input)))
+  (pcase multi-run-term-type
+    ('eshell 'eshell-send-input)
+    ('shell 'comint-send-input)
+    ((pred multi-run-match-term-type-p) 'term-send-input)
+    (multi-run-term-type (error "Value of multi-run-term-type should be one of the following symbols: eshell, shell, ansi-term, term, multi-term"))))
 
-(defun mr-get-new-input-point ()
+(defun multi-run-get-new-input-point ()
   "Move point to the latest prompt in the terminal buffer."
-  (cond ((string= mr-term "eshell") eshell-last-output-end)
-	((string= mr-term "shell") (process-mark (get-buffer-process (current-buffer))))
-	((string= mr-term "ansi-term") (process-mark (get-buffer-process (current-buffer))))
-	((string= mr-term "term") (process-mark (get-buffer-process (current-buffer))))
-	((string= mr-term "multi-term") (process-mark (get-buffer-process (current-buffer))))))
+  (pcase multi-run-term-type
+    ('eshell eshell-last-output-end)
+    ((pred multi-run-match-term-type-p) (process-mark (get-buffer-process (current-buffer))))
+    (multi-run-term-type (error "Value of multi-run-term-type should be one of the following symbols: eshell, shell, ansi-term, term, multi-term"))))
 
-(defun mr-open-terminal (term-num)
+(defun multi-run-open-terminal (term-num)
   "Open terminal number TERM-NUM in a buffer if it's not already open.  In any case, switch to it."
-  (when (not (get-buffer (mr-get-buffer-name term-num)))
+  (when (not (get-buffer (multi-run-get-buffer-name term-num)))
     (progn
-      (cond ((string= mr-term "eshell") (eshell term-num))
-	    ((string= mr-term "shell") (shell))
-	    ((string= mr-term "ansi-term") (ansi-term "/bin/bash"))
-	    ((string= mr-term "term") (term "/bin/bash"))
-	    ((string= mr-term "multi-term") (multi-term)))
-      (rename-buffer (mr-get-buffer-name term-num))))
-  (switch-to-buffer (mr-get-buffer-name term-num)))
+      (pcase multi-run-term-type
+	('eshell (eshell term-num))
+	('shell (shell))
+	('ansi-term (ansi-term "/bin/bash"))
+	('term (term "/bin/bash"))
+	('multi-term (multi-term))
+	(multi-run-term-type (error "Value of multi-run-term-type should be one of the following symbols: eshell, shell, ansi-term, term, multi-term")))
+      (rename-buffer (multi-run-get-buffer-name term-num))))
+  (switch-to-buffer (multi-run-get-buffer-name term-num)))
 
 ;; run a command on a single terminal
-(defun mr-run-on-single-terminal (command term-num)
+(defun multi-run-on-single-terminal (command term-num)
   "Run the command COMMAND on a single terminal with number TERM-NUM."
-  (set-buffer (mr-get-buffer-name term-num))
-  (goto-char (mr-get-new-input-point))
+  (set-buffer (multi-run-get-buffer-name term-num))
+  (goto-char (multi-run-get-new-input-point))
   (insert command)
-  (funcall (mr-get-input-function)))
+  (funcall (multi-run-get-input-function)))
 
 ;; run a command on multiple terminals
-(defun mr-run-on-terminals (command term-nums &optional delay)
+(defun multi-run-on-terminals (command term-nums &optional delay)
   "Run the COMMAND on terminals in TERM-NUMS with an optional DELAY between running on successive terminals."
   (when (not delay)
     (setq delay 0))
-  (setq delay-cnt 0)
-  (setq evaled-command command)
-  (while term-nums
-    (when (functionp command)
-      (setq evaled-command (funcall command (car term-nums))))
-    (setq multi-run-timers (cons (run-at-time (concat (number-to-string (* delay-cnt delay)) " sec") nil
-					      'mr-run-on-single-terminal evaled-command (car term-nums)) multi-run-timers))
-    (setq term-nums (cdr term-nums))
-    (setq delay-cnt (1+ delay-cnt))))
+  (let ((delay-cnt 0))
+    (while term-nums
+      (let ((evaled-command (if (functionp command)
+				(funcall command (car term-nums)) command)))
+	(setq multi-run-timers-list
+	      (cons (run-at-time (concat (number-to-string (* delay-cnt delay)) " sec")
+				 nil
+				 'multi-run-on-single-terminal evaled-command (car term-nums))
+		    multi-run-timers-list))
+	(setq term-nums (cdr term-nums))
+	(setq delay-cnt (1+ delay-cnt))))))
 
-(defun mr-create-terminals (num-terminals)
+(defun multi-run-create-terminals (num-terminals)
   "Create NUM-TERMINALS number of terminals."
   (dotimes (i num-terminals)
-    (mr-open-terminal (1+ i))))
+    (multi-run-open-terminal (1+ i))))
 
-(defun mr-configure-terminals (num-terminals &optional window-batch)
+(defun multi-run-configure-terminals (num-terminals &optional window-batch)
   "Lay out NUM-TERMINALS number of terminals on the screen with WINDOW-BATCH number of them in one single vertical slot."
   (when (not window-batch)
     (setq window-batch 5))
   (setq master-buffer-name (buffer-name))
-  (mr-create-terminals num-terminals)
+  (multi-run-create-terminals num-terminals)
 
-  (defun mr-make-symbols (num-terminals)
-    (defun mr-make-symbols-helper (cnt)
+  (defun multi-run-make-symbols (num-terminals)
+    (defun multi-run-make-symbols-helper (cnt)
       (when (<= cnt num-terminals)
-	(vconcat (vector (make-symbol (concat "term" (number-to-string cnt)))) (mr-make-symbols-helper (1+ cnt)))))
-    (mr-make-symbols-helper 0))
+	(vconcat (vector (make-symbol (concat "term" (number-to-string cnt)))) (multi-run-make-symbols-helper (1+ cnt)))))
+    (multi-run-make-symbols-helper 0))
 
-  (defun mr-make-dict (num-terminals)
-    (defun mr-make-dict-helper (cnt)
+  (defun multi-run-make-dict (num-terminals)
+    (defun multi-run-make-dict-helper (cnt)
       (when (<= cnt num-terminals)
 	(cons (list :name (aref sym-vec cnt)
-		    :buffer (mr-get-buffer-name cnt))
-	      (mr-make-dict-helper (1+ cnt)))))
-    (mr-make-dict-helper 1))
+		    :buffer (multi-run-get-buffer-name cnt))
+	      (multi-run-make-dict-helper (1+ cnt)))))
+    (multi-run-make-dict-helper 1))
 
-  (setq sym-vec (mr-make-symbols num-terminals))
+  (setq sym-vec (multi-run-make-symbols num-terminals))
   (setq dict (cons (list :name (aref sym-vec 0)
 			 :buffer master-buffer-name)
-		   (mr-make-dict num-terminals)))
+		   (multi-run-make-dict num-terminals)))
   
   (defun make-vertical-or-horizontal-pane (num-terminals offset choice)
     (if (= num-terminals 1) (aref sym-vec offset)
@@ -138,27 +175,27 @@
    overall-recipe
    dict)
   (select-window (get-buffer-window master-buffer-name))
-  (setq mr-terminals (number-sequence 1 num-terminals))
-  (concat "Preemptively setting mr-terminals to " (prin1-to-string mr-terminals)))
+  (setq multi-run-terminals-list (number-sequence 1 num-terminals))
+  (concat "Preemptively setting multi-run-terminals-list to " (prin1-to-string multi-run-terminals)))
 
 (defun multi-run-with-delay (delay &rest cmd)
   "With the provided DELAY, run one or more commands CMD on multiple terminals - the delay is between running commands on different terminals."
-  (when (not (boundp (quote mr-terminals)))
-    (error "Define the variable mr-terminals on which you want to run the command on.  e.g. (list 1 2 3)"))
-  (setq delay-now 0)
-  (dolist (command cmd)
-    (setq multi-run-timers (cons (run-at-time (concat (number-to-string delay-now) " sec") nil 'mr-run-on-terminals command mr-terminals delay) multi-run-timers))
-    (setq delay-now (+ delay-now (* (length mr-terminals) delay))))
+  (when (not (boundp (quote multi-run-terminals-list)))
+    (error "Define the variable multi-run-terminals-list on which you want to run the command on.  e.g. (list 1 2 3)"))
+  (let ((delay-now 0))
+    (dolist (command cmd)
+      (setq multi-run-timers-list (cons (run-at-time (concat (number-to-string delay-now) " sec") nil 'multi-run-on-terminals command multi-run-terminals-list delay) multi-run-timers-list))
+      (setq delay-now (+ delay-now (* (length multi-run-terminals-list) delay)))))
   nil)
 
 (defun multi-run-with-delay2 (delay &rest cmd)
   "With the provided DELAY, run one or more commands CMD on multiple terminals - but the delay is between different command invocations at the terminals."
-  (when (not (boundp (quote mr-terminals)))
-    (error "Define the variable mr-terminals on which you want to run the command on.  e.g. (list 1 2 3)"))
-  (setq delay-now 0)
-  (dolist (command cmd)
-    (setq multi-run-timers (cons (run-at-time (concat (number-to-string delay-now) " sec") nil 'mr-run-on-terminals command mr-terminals) multi-run-timers))
-    (setq delay-now (+ delay-now delay)))
+  (when (not (boundp (quote multi-run-terminals-list)))
+    (error "Define the variable multi-run-terminals-list on which you want to run the command on.  e.g. (list 1 2 3)"))
+  (let ((delay-now 0))
+    (dolist (command cmd)
+      (setq multi-run-timers-list (cons (run-at-time (concat (number-to-string delay-now) " sec") nil 'multi-run-on-terminals command multi-run-terminals-list) multi-run-timers-list))
+      (setq delay-now (+ delay-now delay))))
   nil)
 
 (defun multi-run (&rest cmd)
@@ -171,35 +208,37 @@
     (setq times 1))
   (when (not delay)
     (setq delay 0))
-  (setq delay-cnt 0)
-  (dotimes (i times)
-    (progn
-      (setq multi-run-timers (cons (run-at-time (concat (number-to-string (* delay-cnt delay)) " sec")
-						nil '(lambda (cmd) (multi-run cmd)) cmd) multi-run-timers))
-      (setq delay-cnt (1+ delay-cnt)))))
+  (let ((delay-cnt 0))
+    (dotimes (i times)
+      (progn
+	(setq multi-run-timers-list (cons (run-at-time (concat (number-to-string (* delay-cnt delay)) " sec")
+						  nil '(lambda (cmd) (multi-run cmd)) cmd) multi-run-timers-list))
+	(setq delay-cnt (1+ delay-cnt))))))
 
 ;; convenience function for ssh'ing to the terminals
 (defun multi-run-ssh (&optional terminal-num)
   "Establish ssh connections in the terminals (or on terminal number TERMINAL-NUM) with the help of user-defined variables."
-  (when (not (boundp (quote mr-terminals)))
-    (error "Define the variable mr-terminals on which you want to run the command on.  e.g. (list 1 2 3)"))
-  (when (not (boundp (quote mr-hostnames-list)))
-    (error "Define the variable mr-hostnames-list to be the list of ip-addrs e.g. (list \"128.84.139.10\" \"128.84.139.11\" \"128.84.139.12\")"))
-  (mr-run-on-terminals (lambda (x) (concat "ssh " (if (boundp (quote ssh-username)) (concat ssh-username "@") "") (elt mr-hostnames-list (- x 1)))) (if terminal-num (list terminal-num) mr-terminals)))
+  (when (not (boundp (quote multi-run-terminals-list)))
+    (error "Define the variable multi-run-terminals-list on which you want to run the command on.  e.g. (list 1 2 3)"))
+  (when (not (boundp (quote multi-run-hostnames-list)))
+    (error "Define the variable multi-run-hostnames-list to be the list of ip-addrs e.g. (list \"128.84.139.10\" \"128.84.139.11\" \"128.84.139.12\")"))
+  (multi-run-on-terminals (lambda (x) (concat "ssh " (if (boundp (quote ssh-username)) (concat ssh-username "@") "") (elt multi-run-hostnames-list (- x 1)))) (if terminal-num (list terminal-num) multi-run-terminals-list)))
 
-(defun mr-kill-terminals (&optional terminal-num)
+(defun multi-run-kill-terminals (&optional terminal-num)
   "Kill terminals (or the optional terminal TERMINAL-NUM)."
-  (when (not (boundp (quote mr-terminals)))
-    (error "Define the variable mr-terminals on which you want to run the command on.  e.g. (list 1 2 3)"))
+  (when (not (boundp (quote multi-run-terminals-list)))
+    (error "Define the variable multi-run-terminals-list on which you want to run the command on.  e.g. (list 1 2 3)"))
   (if terminal-num
-      (kill-buffer (mr-get-buffer-name terminal-num))
-    (mapc (lambda (terminal-num) (kill-buffer (mr-get-buffer-name terminal-num))) mr-terminals))
+      (kill-buffer (multi-run-get-buffer-name terminal-num))
+    (mapc (lambda (terminal-num) (kill-buffer (multi-run-get-buffer-name terminal-num))) multi-run-terminals-list))
   nil)
 
-(defun mr-kill-all-timers ()
+(defun multi-run-kill-all-timers ()
   "Cancel commands running on a loop or via delay functions."
-  (mapc (lambda (timer) (cancel-timer timer)) multi-run-timers)
+  (mapc (lambda (timer) (cancel-timer timer)) multi-run-timers-list)
   "All timers canceled")
 
+
 (provide 'multi-run)
-;;; multi-run ends here
+
+;;; multi-run.el ends here
